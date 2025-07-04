2811366a9212016d0ccf64c16e6ed293a1bafc8e
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-07-04 13:01:02 +0600

    display-all-todos

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchTodos } from "./RTK/Features/TODO/todoSlice";
 
 export default function App() {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
   const dispatch = useDispatch();
   const todos = useSelector((state) => state.todoSlice);
@@ -35,9 +35,58 @@ export default function App() {
         </button>
       </div>
 
-      {loading && (
+      {todos.isLoading && (
         <div className="w-10 h-10 border-4 border-t-blue-500 border-gray-300 rounded-full animate-spin mx-auto my-10" />
       )}
+
+      <div className="grid grid-cols-4 gap-5 my-10">
+        {todos.todos.map((todo) => (
+          <div className="flex flex-col gap-2 w-full text-[10px] sm:text-xs z-50">
+            <div className="succsess-alert cursor-default flex items-center justify-between w-full h-12 sm:h-14 rounded-lg bg-[#232531] px-[10px]">
+              <div className="flex gap-2">
+                <div className="text-[#2b9875] bg-white/5 backdrop-blur-xl p-1 rounded-lg">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 12.75 6 6 9-13.5"
+                    />
+                  </svg>
+                </div>
+                <div>
+                  <p className="text-white">done successfully :)</p>
+                  <p className="text-gray-500">
+                    This is the description section
+                  </p>
+                </div>
+              </div>
+              <button className="text-gray-600 hover:bg-white/5 p-1 rounded-md transition-colors ease-linear">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
